b5fc189cb8227144cc6f89019f7c3a960d69801e
juliocj360 <juliocj360@gmail.com>
2017-02-26 18:08:33 -0800

    Removing comments. #8

--- key.js
+++ key.js
@@ -11,33 +11,3 @@ config.password = atom.config.get('spoken-code.password', (newValue) => {
 })
 
 module.exports = config
-/*
-atom.config.observe('spoken-code.userName', (newValue) => {
-  userName = newValue
-})
-
-atom.config.observe('spoken-code.password', (newValue) => {
-  password = newValue
-})
-
-module.exports = {
-  "url": "https://stream.watsonplatform.net/text-to-speech/api",
-  "username": userName,
-  "password": password
-} */
-
-/*
-let userName = atom.config.get('spoken-code.userName', (newValue) => {
-  return newValue
-})
-
-let password = atom.config.get('spoken-code.password', (newValue) => {
-  return newValue
-})
-
-const config = {
-  "url": "https://stream.watsonplatform.net/text-to-speech/api",
-  "username": userName,
-  "password": password
-}
-*/
