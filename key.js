1a8d857feffdb85e6620af8c0de1dcab90afa80d
juliocj360 <juliocj360@gmail.com>
2017-02-28 23:45:13 -0800

    Patch to fix settings bugs. #16

--- key.js
+++ key.js
@@ -1,13 +1,16 @@
+atom.config.get('spoken-code.userName')
+atom.config.get('spoken-code.password')
+
 const apiInfo = {
   url: "https://stream.watsonplatform.net/text-to-speech/api"
 }
 
 atom.config.observe('spoken-code.userName', (newValue) => {
-  newValue.length < 1 ? apiInfo.username = ' ' : apiInfo.username = newValue
+  newValue.length >= 1 ? apiInfo.username = newValue : atom.config.set('spoken-code.userName', " ")
 })
 
 atom.config.observe('spoken-code.password', (newValue) => {
-  newValue.length < 1 ? apiInfo.password = ' ' : apiInfo.password = newValue
+  newValue.length >= 1 ? apiInfo.password = newValue : atom.config.set('spoken-code.password', " ")
 })
 
 module.exports = apiInfo
